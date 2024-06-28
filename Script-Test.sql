47a11b73ed9e25e52e3742102a8a785b52dffbf7
gcasado0 <casado.gustavo@gmail.com>
2024-06-28 08:51:42 -0300

    Mejora en testing y correccion de datos de asientos

--- Script-Test.sql
+++ Script-Test.sql
@@ -9,12 +9,39 @@ select dbo.GetBandaId(nombre) as ID from banda;
 select dbo.GetTotalRecaudado(1, '2024-01-01', '2024-06-30') as Total;
 select dbo.GetTotalRecaudado(1, '2024-07-01', '2024-12-31') as Total;
 
+
+-- Test script 7 - Venta -----------------------------------------------------------------------
+-- Caso exitoso
+DECLARE @DNI VARCHAR(10) = '56789012O';
+DECLARE @Fecha DATE = GETDATE();
+DECLARE @VentaID INT;
+EXEC usp_venta @DNI, @Fecha, @VentaID OUTPUT;
+SELECT @VentaID
+
+-- Caso de cliente inexistente
+DECLARE @DNI VARCHAR(10) = '56789012OF';
+DECLARE @Fecha DATE = GETDATE();
+DECLARE @VentaID INT;
+EXEC usp_venta @DNI, @Fecha, @VentaID OUTPUT;
+SELECT @VentaID
+
+-- Test script 7 - Entrada -----------------------------------------------------------------------
+-- Caso exitoso
+DECLARE @VentaID INT = 31;
+DECLARE @recital_id INT = 1;
+EXEC usp_entrada @VentaID, @recital_id, 20;
+
+-- Caso de asiento inexistente
+DECLARE @VentaID INT = 31;
+DECLARE @recital_id INT = 1;
+EXEC usp_entrada @VentaID, @recital_id, 180;
+
 -- Test script 7 -----------------------------------------------------------------------
 
 DECLARE @tabla_asiento dbo.TipoTablaAsiento;
 
 INSERT INTO @tabla_asiento (asiento_id)
-VALUES (10), (14), (13);
+VALUES (11), (15), (16);
 
 DECLARE @DNI VARCHAR(10) = '56789012O';
 DECLARE @Fecha DATE = GETDATE();
@@ -27,7 +54,7 @@ EXEC usp_compra @DNI, @Fecha, @recital_id, @tabla_asiento;
 DECLARE @tabla_asiento dbo.TipoTablaAsiento;
 
 INSERT INTO @tabla_asiento (asiento_id)
-VALUES (1), (2), (300);
+VALUES (300);
 
 DECLARE @Fecha DATE = GETDATE();
 
@@ -38,7 +65,7 @@ EXEC usp_compra '12345678K', @Fecha, 1, @tabla_asiento;
 DECLARE @tabla_asiento dbo.TipoTablaAsiento;
 
 INSERT INTO @tabla_asiento (asiento_id)
-VALUES (1), (2);
+VALUES (300);
 
 DECLARE @Fecha DATE = GETDATE();
 
